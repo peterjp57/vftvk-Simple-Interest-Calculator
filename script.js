39273516b6527f3234d2ced83f81450d60f09df9
peterjp57 <peterjp57@outlook.com>
2021-11-12 17:46:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ function compute()
     p = document.getElementById("principal").value;
     
 }
+    <script>
+     var principal = document.getElementById("principal").value;
+     var rate = document.getElementById("rate").value;
+     var years = document.getElementById("years").value;
+     var interest = principal * years * rate /100;
+     var year = new Date().getFullYear()+parseInt(years);
+
+    </script>    
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
         
